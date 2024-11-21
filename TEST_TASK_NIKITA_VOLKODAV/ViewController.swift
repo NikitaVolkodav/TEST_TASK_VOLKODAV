274e9572f414f1fd021c417eb7860e6fe00242a3
nikitaVolkodav <ni.vo@dcjudgement.com>
2024-11-21 16:24:17 +0200

    add-project

---
+++ TEST_TASK_NIKITA_VOLKODAV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TEST_TASK_NIKITA_VOLKODAV
+//
+//  Created by Nikita Volkodav  on 21.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
